b4eb834eb8665297aed9dd3f85171260c1002234
leoquilmes88 <leoquilmes88@gmail.com>
2021-10-19 18:55:43 -0300

    app en ambas clases de estacionamiento por app

--- src/estacionamiento/EstacionamientoFinalizadoAPP.java
+++ src/estacionamiento/EstacionamientoFinalizadoAPP.java
@@ -2,15 +2,23 @@ package estacionamiento;
 
 import java.time.LocalTime;
 
+import app.APP;
+
 public class EstacionamientoFinalizadoAPP extends Estacionamiento {
 
+	private APP aplicacion;
 	private LocalTime horaFin;
 	private Float costo;
 
-	public EstacionamientoFinalizadoAPP(LocalTime horaInicio, LocalTime horaFin, String patente, Float costo) {
+	public EstacionamientoFinalizadoAPP(APP aplicacion, LocalTime horaInicio, LocalTime horaFin, String patente, Float costo) {
 		super(horaInicio, patente);
+		this.aplicacion = aplicacion;
 		this.horaFin = horaFin;
 		this.costo = costo;
+	}
+	
+	public APP getApp() {
+		return this.aplicacion;
 	}
 
 	public LocalTime getHoraFin() {
--- test/estacionamiento/EstacionamientoFinalizadoAPPTest.java
+++ test/estacionamiento/EstacionamientoFinalizadoAPPTest.java
@@ -1,27 +1,38 @@
 package estacionamiento;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
 
 import java.time.LocalTime;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import app.APP;
+
 class EstacionamientoFinalizadoAPPTest extends EstacionamientoTest {
 
+	private APP aplicacion;
 	private LocalTime horaFin;
 	private Float costo;
 	
 	@BeforeEach
 	void setUp() throws Exception {
 	
+		this.aplicacion = mock(APP.class);
 		this.horaInicio = LocalTime.of(12, 0);
 		this.horaFin = LocalTime.of(15, 0);
 		this.patente = "AA 325 AA";
 		this.costo = 120f;
-		this.estacionamiento = new EstacionamientoFinalizadoAPP(this.horaInicio, this.horaFin, this.patente, 
-				this.costo);
+		this.estacionamiento = new EstacionamientoFinalizadoAPP(this.aplicacion, this.horaInicio, 
+				this.horaFin, this.patente, this.costo);
 		
+	}
+	
+	@Test
+	void testTieneUnaAPPAsociada() {
+		APP aplicacionEsperada = this.aplicacion;
+		assertEquals(aplicacionEsperada, ((EstacionamientoFinalizadoAPP) this.estacionamiento).getApp());
 	}
 
 	@Test
