c4c080ef45056b3de98339b7a5a96598b4fa3967
leonelmontiel <leonel.montiel@outlook.com>
2021-10-28 18:54:45 -0300

    test getNumero clase App

--- src/app/App.java
+++ src/app/App.java
@@ -2,4 +2,14 @@ package app;
 
 public class App {
 
+	private int numero;
+
+	public App(int numero) {
+		this.numero = numero;
+	}
+
+	public int getNumero() {
+		return this.numero;
+	}
+
 }
--- test/app/AppTest.java
+++ test/app/AppTest.java
@@ -7,13 +7,18 @@ import org.junit.jupiter.api.Test;
 
 class AppTest {
 
+	private App app;
+
 	@BeforeEach
 	void setUp() throws Exception {
+		this.app = new App(1150625347);
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testGetNumero() {
+		int numEsperado = 1150625347;
+		int numObtenido = this.app.getNumero();
+		assertEquals(numEsperado, numObtenido);
 	}
 
 }
