4afb3eaeafcd5c13fa3b1e8b8d87dbdfa2e5a42d
leoquilmes88 <leoquilmes88@gmail.com>
2021-11-04 19:36:52 -0300

    error en test iniciarEstacionamiento

--- src/sem/SEM.java
+++ src/sem/SEM.java
@@ -1,11 +1,50 @@
 package sem;
 
 import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import app.APP;
+import estacionamiento.Estacionamiento;
+import estacionamiento.EstacionamientoAPP;
 
 public class SEM {
 
+	private LocalTime horaInicio;
+	private LocalTime horaCierre;
+	private Float precioPorHora;
+	private List<Estacionamiento> estacionamientos;
+
 	public SEM(LocalTime horaInicio, LocalTime horaCierre, Float precioPorHora) {
-		// TODO Auto-generated constructor stub
+		this.horaInicio = horaInicio;
+		this.horaCierre = horaCierre;
+		this.precioPorHora = precioPorHora;
+		this.estacionamientos = new ArrayList<Estacionamiento>();
+	}
+
+	public LocalTime getHoraInicio() {
+		return this.horaInicio;
+	}
+
+	public LocalTime getHoraCierre() {
+		return this.horaCierre;
+	}
+
+	public Float getPrecioPorHora() {
+		return this.precioPorHora;
+	}
+
+	void setEstacionamientos(List<Estacionamiento> estacionamientos) {
+		this.estacionamientos = estacionamientos;
+	}
+
+	public Estacionamiento iniciarEstacionamiento(String patente, APP app) {
+		LocalTime horaInicio = LocalTime.now(); 
+		EstacionamientoAPP nuevoEstacionamiento = new EstacionamientoAPP(app, horaInicio, patente);
+		
+		this.estacionamientos.add(nuevoEstacionamiento);
+		
+		return nuevoEstacionamiento;
 	}
 
 }
--- test/sem/SEMTest.java
+++ test/sem/SEMTest.java
@@ -1,11 +1,16 @@
 package sem;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import app.APP;
+import estacionamiento.Estacionamiento;
 
 class SEMTest {
 
@@ -14,19 +19,52 @@ class SEMTest {
 	private LocalTime horaCierre;
 	private Float precioPorHora;
 	
+	private APP app;
+	
 	@BeforeEach
 	void setUp() throws Exception {
-	
+		//mocks
+		this.app = mock(APP.class);
+		
 		this.horaInicio = LocalTime.of(7, 0);
 		this.horaCierre = LocalTime.of(20, 0);
 		this.precioPorHora = 40f;
+		
 		this.sem = new SEM(this.horaInicio, this.horaCierre, this.precioPorHora);
 		
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testHoraInicio() {
+		LocalTime horaEsperada = this.horaInicio;
+		
+		assertEquals(horaEsperada, this.sem.getHoraInicio());
+	}
+	
+	@Test
+	void testHoraCierre() {
+		LocalTime horaEsperada = this.horaCierre;
+		
+		assertEquals(horaEsperada, this.sem.getHoraCierre());
+	}
+	
+	@Test
+	void testPrecioPorHora() {
+		Float precioEsperado = 40f;
+		
+		assertEquals(precioEsperado, this.sem.getPrecioPorHora());
 	}
 
+	@Test
+	void testIniciarEstacionamiento() {
+		//setup 
+		List<Estacionamiento> estacionamientosSpy = spy(new ArrayList<Estacionamiento>());
+		this.sem.setEstacionamientos(estacionamientosSpy);
+		
+		Estacionamiento estacionamientoNuevo = this.sem.iniciarEstacionamiento("abc 123", this.app);
+		
+		//verify
+		verify(estacionamientosSpy).add(estacionamientoNuevo);
+	}
+	
 }
